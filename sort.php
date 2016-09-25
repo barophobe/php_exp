d3976bf9c1b14c41f7906bbf5aee040c551ef3b7
Ed Connolly <edmond.connolly@webinnovate.ie>
2016-09-25 14:13:28 +0100

    add first file

---
+++ sort.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset ="utf-8">
+	<title>Sorting Multidimensional Arrays</title>
+	<link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+<?php # script 1.1 - sort.php
+
+// this page creates a multidimensional array of names and grades.
+// the array is then sorted twice, once by name then by grade.#
+
+//create the array
+//array structure:
+// studentID => array('name' => 'Name', 'grade' => XX.X)
+
+$students = array(
+	256 => array('name' => 'John', 'grade' => 98.5 ),
+	256 => array('name' => 'John', 'grade' => 98.5 ),
+	256 => array('name' => 'John', 'grade' => 98.5 ),
+	256 => array('name' => 'John', 'grade' => 98.5 ),
+	256 => array('name' => 'John', 'grade' => 98.5 )
+	);
+
+//name the sorting function
+function name_sort($x, $y) {
+	return strcasecmp($x['name'], $y['name']);
+}
+	
+//grade sorting function
+function grade_sort($x, $y) {
+	return ($x['name'] < $y['name']);
+}
+
+echo '<h2> Array As Is</h2><pre>' .print_r($students, 1) . '</pre>';
+
+//sort by name
+uasort($students, name_sort);
+echo '<h2> Array Sorted By Name</h2><pre>' .print_r($students, 1) . '</pre>';
+
+uasort($students, grade_sort);
+echo '<h2> Array Sorted By Grade</h2><pre>' .print_r($students, 1) . '</pre>';
+
+?>
+
+</body>
+</html>
